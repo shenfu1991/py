0b6f955ead479d988730a53fc9621f7dc1be657e
xuanyuan <xuanyuanshenfu@outlook.com>
2023-07-08 17:49:42 +0800

    add

--- ls.py
+++ ls.py
@@ -13,8 +13,8 @@ for filename in os.listdir(folder_path):
         df = pd.read_csv(file_path)
 
         # 将'result'列的'long'和'LN'替换为'L'，'short'和'SN'替换为'S'
-        df['result'] = df['result'].replace(['long', 'LN'], 'L')
-        df['result'] = df['result'].replace(['short', 'SN'], 'S')
+        df['result'] = df['result'].replace(['LN'], 'long')
+        df['result'] = df['result'].replace(['SN'], 'short')
 
         # 保存新的csv文件
         new_file_path = os.path.join(folder_path, 'new_' + filename)
