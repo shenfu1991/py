e373a05168457c7c350f39e7124a1c2787dd5813
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-08 17:49:26 +0800

    u

--- ns.py
+++ ns.py
@@ -5,44 +5,44 @@ from joblib import load
 app = Flask(__name__)
 
 # 加载模型和 LabelEncoder
-model_3ma2 = load('model_3ma2.joblib')
-model_5ma2 = load('model_5ma2.joblib')
-model_15ma2 = load('model_15ma2.joblib')
-model_30ma2 = load('model_30ma2.joblib')
-model_1ha2 = load('model_1ha2.joblib')
-model_4ha2 = load('model_4ha2.joblib')
-le3ma2 = load('label_encoder_3ma2.joblib')
-le5ma2 = load('label_encoder_5ma2.joblib')
-le15ma2 = load('label_encoder_15ma2.joblib')
-le30ma2 = load('label_encoder_30ma2.joblib')
-le1ha2 = load('label_encoder_1ha2.joblib')
-le4ha2 = load('label_encoder_4ha2.joblib')
+model_3v4 = load('model_3v4.joblib')
+model_5v4 = load('model_5v4.joblib')
+model_15v4 = load('model_15v4.joblib')
+model_30v4 = load('model_30v4.joblib')
+model_1v4 = load('model_1v4.joblib')
+model_4v4 = load('model_4v4.joblib')
+le3v4 = load('label_encoder_3v4.joblib')
+le5v4 = load('label_encoder_5v4.joblib')
+le15v4 = load('label_encoder_15v4.joblib')
+le30v4 = load('label_encoder_30v4.joblib')
+le1v4 = load('label_encoder_1v4.joblib')
+le4v4 = load('label_encoder_4v4.joblib')
 
 
 
 @app.route('/predict_3m', methods=['POST'])
 def predict_3m():
-    return predict(model_3ma2,le3ma2)
+    return predict(model_3v4,le3v4)
 
 @app.route('/predict_5m', methods=['POST'])
 def predict_5m():
-    return predict(model_5ma2,le5ma2)
+    return predict(model_5v4,le5v4)
 
 @app.route('/predict_15m', methods=['POST'])
 def predict_15m():
-    return predict(model_15ma2,le15ma2)
+    return predict(model_15v4,le15v4)
 
 @app.route('/predict_30m', methods=['POST'])
 def predict_30m():
-    return predict(model_30ma2,le30ma2)
+    return predict(model_30v4,le30v4)
 
 @app.route('/predict_1h', methods=['POST'])
 def predict_1h():
-    return predict(model_1ha2,le1ha2)
+    return predict(model_1v4,le1v4)
 
 @app.route('/predict_4h', methods=['POST'])
 def predict_4h():
-    return predict(model_4ha2,le4ha2)
+    return predict(model_4v4,le4v4)
 
 
 
