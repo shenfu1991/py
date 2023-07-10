f349349985b91085eb55d73daf248bd633f66a34
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-10 10:33:14 +0800

    add

---
+++ del_empty.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+# 指定你的文件夹路径
+folder_path = '/Users/xuanyuan/Downloads/e'
+
+# 遍历文件夹中的每一个文件
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(folder_path, filename)
+        
+        # 读取CSV文件
+        df = pd.read_csv(file_path)
+        
+        # 删除result列中为空的行
+        df = df[df['result'].notna()]
+        
+        # 将结果写回CSV文件
+        df.to_csv(file_path, index=False)
