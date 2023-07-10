0b3313417006cae62b25d7f556b51fc015d5ee51
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-10 11:32:01 +0800

    add

---
+++ del_simi.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+
+# The directory where the CSV files are stored
+directory = '/Users/xuanyuan/Downloads/e'
+
+# Loop over all files in the directory
+for filename in os.listdir(directory):
+    # Check if the file is a CSV file
+    if filename.endswith('.csv'):
+        # Full path to the CSV file
+        filepath = os.path.join(directory, filename)
+        
+        # Load the data from the CSV file
+        data = pd.read_csv(filepath)
+
+        # Remove 'result' column from consideration
+        data_without_result = data.drop(columns=['result'])
+
+        # Create a copy of the original DataFrame to preserve the original data
+        data_cleaned = data.copy()
+
+        # Shift the DataFrame down by one row
+        data_shifted = data_without_result.shift(-1)
+
+        # Count the number of columns where the original and shifted DataFrames have the same value
+        num_same_values = (data_without_result == data_shifted).sum(axis=1)
+
+        # Find the rows where 4 or more columns have the same value in the original and shifted DataFrames
+        rows_to_remove = num_same_values >= 7
+
+        # Remove these rows from the cleaned DataFrame
+        data_cleaned = data_cleaned[~rows_to_remove]
+
+        # Save the cleaned data to a new CSV file
+        cleaned_filepath = os.path.join(directory, 'cleaned_' + filename)
+        data_cleaned.to_csv(cleaned_filepath, index=False)
+
+        # Print the shape of the cleaned DataFrame
+        print(f'{filename}: {data_cleaned.shape}')
