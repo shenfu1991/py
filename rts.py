37c8d1300abb43b779ece1eebdaee948f896f048
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-26 18:01:58 +0800

    add

---
+++ rts.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+# Load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the POST request
+    data = request.get_json(force=True)
+
+    # Convert the data into numpy array
+    predict_request = np.array(data['input'])
+
+    # Use the model to predict
+    prediction = model.predict(predict_request.reshape(1, -1))
+
+    # Convert numpy array to list
+    output = prediction.tolist()
+
+    # Return the result
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
