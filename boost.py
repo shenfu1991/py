1d8345341284e620f7a137acc867ee8e5383fcef
xuanyuan <xuanyuanshenfu@outlook.com>
2023-08-15 09:19:00 +0800

    add

--- boost.py
+++ boost.py
@@ -10,7 +10,7 @@ from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
 
 # Load the data
-data_path = '/Users/xuanyuan/py/merged_3_r.csv'  # Replace with your actual path
+data_path = '/Users/xuanyuan/py/merged_6_r.csv'  # Replace with your actual path
 print(data_path)
 data = pd.read_csv(data_path)
 
