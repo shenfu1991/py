a7ef5346b37c1919f829e78ad12fbf8bbb189d82
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-09-06 14:22:10 +0800

    update

--- ls.py
+++ ls.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.optimizers import Adam
@@ -13,9 +13,9 @@ data = pd.read_csv('merged_5m-r.csv')
 X = data.drop('result', axis=1)
 y = data['result']
 
-# Encode the target column
-label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
+# One-hot encode the target column
+onehot_encoder = OneHotEncoder(sparse=False)
+y_encoded = onehot_encoder.fit_transform(y.values.reshape(-1, 1))
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
@@ -33,9 +33,9 @@ X_test_reshaped = X_test_scaled.reshape(X_test_scaled.shape[0], 1, X_test_scaled
 model = Sequential()
 model.add(LSTM(50, input_shape=(X_train_reshaped.shape[1], X_train_reshaped.shape[2]), activation='relu', return_sequences=True))
 model.add(LSTM(50, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))  # Assuming binary classification
+model.add(Dense(4, activation='softmax'))  # 4 units for 4 categories
 
-model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # 4. Train the model
 model.fit(X_train_reshaped, y_train, epochs=10, batch_size=32, validation_data=(X_test_reshaped, y_test), verbose=1)
